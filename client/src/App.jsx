018f0bff0ee41b9de551eb4c297da29f9afcaca0
Yousef tarekelgohary <yousefelgohary455@gmail.com>
2023-06-28 07:28:42 +0300

    🌑 adjustments

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,18 @@
 import './App.css'
+import Navbar from 'pages/Navbar'
+import Home from 'pages/Home'
+import Login from 'pages/Login'
+import Profile from 'pages/Profile'
 
 function App() {
 
 
   return (
     <div className='app'>
-      hello world
+      <Navbar/>
+      <Home />
+      <Profile />
+      <Login/>
     </div>
   )
 }
