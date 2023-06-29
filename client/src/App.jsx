6d9897023b1bfb811283a021fc9d9fd2607a22da
Yousef tarekelgohary <yousefelgohary455@gmail.com>
2023-06-29 20:48:28 +0300

    🌑 adjust

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,8 @@
 import './App.css'
-import Navbar from 'pages/Navbar'
-import Home from 'pages/Home'
-import Login from 'pages/Login'
-import Profile from 'pages/Profile'
+import Navbar from './pages/Navbar'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Profile from './pages/Profile'
 
 function App() {
 
