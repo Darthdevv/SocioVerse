bc26ff3c23c2b734a0219d9ee9f80c65db5a5f41
Yousef tarekelgohary <yousefelgohary455@gmail.com>
2023-07-02 01:53:48 +0300

    🌑 theme  added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,29 @@
 import './App.css'
-import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Profile from './pages/Profile'
+import { useMemo } from 'react'
+import { useSelector } from 'react-redux'
+import { ThemeProvider, CssBaseline } from '@mui/material'
+import { themeSettings } from 'theme'
+import { createTheme } from '@mui/material'
 
 function App() {
-
+  const mode = useSelector(state => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)),[mode]);
 
   return (
     <div className="app">
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/profile/:userId" element={<Profile />} />
-        </Routes>
+        <ThemeProvider theme={theme}>
+          <CssBaseline/>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/profile/:userId" element={<Profile />} />
+          </Routes>
+        </ThemeProvider>
       </BrowserRouter>
     </div>
   );
